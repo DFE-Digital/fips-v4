ed3dd7272635f7ecfd4d062c32995a559d84dff2
Andy Jones <andy.jones@digital.education.gov.uk>
2025-08-29 10:36:49 +0100

    Initial;

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,17 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+// Import controllers
+const productsController = require('./controllers/products')
+
+// Products routes
+router.get('/products', productsController.index)
+router.get('/product/:id', productsController.show)
+router.get('/product/:id/categories', productsController.categories)
+
+// About page route
+router.get('/about', (req, res) => {
+    res.render('about/index')
+})
+
 // Add your routes here
